7ca90dc9053d360f16a3334bac10ba18fea0b389
Iryna Vlasenko <via-lilly@ukr.net>
2021-12-12 20:42:09 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+            resolve({ position, delay });
+            }, delay);
+        }); 
+    } else {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+            reject({ position, delay });
+            }, delay);
+        });
+    }
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const formElement = e.currentTarget.elements;
+  const delayEl = formElement.delay.valueAsNumber;
+  const stepEl = formElement.step.valueAsNumber;
+  const amountEl = formElement.amount.valueAsNumber;
+
+  if (amountEl > 0) {
+    for (let i = 0; i < amountEl; i += 1) {
+      let newDelay = delayEl;
+      newDelay = delayEl + stepEl * i;
+
+      createPromise(i + 1, newDelay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
   }
 }
